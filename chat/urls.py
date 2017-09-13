9c1ec73b3ce3b8d8cbc4802ccdcec4d7ae318d99
kallepallimounikasmitha <k.mounikasmitha999@gmail.com>
2017-09-13 20:15:35 +0530

    chatapp

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
+from django.urls import reverse
 
 from chat import views
 
 urlpatterns = [
                        url(r'^$', views.index, name='index'),
-                       url(r'^(?P<pk>[0-9]+)/$', views.chat_room, name='chat_room'),
+                       url(r'^(?P<chat_room_id>[0-9]+)/$', views.chat_room, name='chat_room'),
+
               ]
